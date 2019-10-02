d11cbd39cbca281be6890ac23064c4bd06f6d9c7
Aparajit Ananya <aparajit.ananya@cloudreach.com>
2019-10-02 17:32:50 -0500

    output website URL when syncing

--- 1-webotron/webotron/webotron.py
+++ 1-webotron/webotron/webotron.py
@@ -25,7 +25,7 @@ bucket_manager = None
 def cli(profile):
     """Webotron deploys websites to AWS."""
     global session, bucket_manager
-    
+
     session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
@@ -66,6 +66,7 @@ def setup_bucket(bucket):
 def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
+    print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
 
 
 if __name__ == '__main__':
